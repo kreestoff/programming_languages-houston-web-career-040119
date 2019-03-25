9d022e13ec866c76eea2bd5cd3c48db2b5d21bd0
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-25 21:48:24 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,1 @@
 new_hash = {}
-def reformat_languages(languages)
-  languages.each do |style, language|
-    language.each do |name, attributes|
-      new_hash[name] ||= attributes
-      new_hash[name][:style] = []
-      new_hash[name][:style] << style
-    end
-  end
-  new_hash
-end
