0a5028388c746907683b76648dbc382a386b4594
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-25 21:52:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,14 @@
-new_hash = {}
 def reformat_languages(languages)
-  new_languages = {}
+  new_hash = {}
   languages.each do |style, language|
     language.each do |language_name, attribute|
-      if new_languages[language_name] == nil
-          new_languages[language_name] = attribute
+      if new_hash[language_name] == nil
+          new_hash[language_name] = attribute
         end
-      new_languages[language_name][:style] = []
-      new_languages[language_name][:style] << style
+      new_hash[language_name][:style] = []
+      new_hash[language_name][:style] << style
     end
   end
-  new_languages[:javascript][:style] << :oo
-  new_languages
+  new_hash[:javascript][:style] << :oo
+  new_hash
 end
